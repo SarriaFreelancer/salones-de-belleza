5b9b2992fa67468e82587cd60911e5bfce484611
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-15 03:56:20 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import ClientOnly from '@/components/client-only';
 import BookingForm from '@/components/public/booking-form';
 import { services } from '@/lib/data';
