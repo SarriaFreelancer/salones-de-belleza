ed70a0162eaa3eba8aceebb6454d9bc8a0b26513
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-15 04:52:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5708924923-1285a",
+  "appId": "1:158901430378:web:3ebcf9a945063ca4f48213",
+  "apiKey": "AIzaSyDaNJQttSAEsBq0IeCDM-qJbSS-v7gfOMk",
+  "authDomain": "studio-5708924923-1285a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "158901430378"
+};
