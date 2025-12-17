ee2aa7fa7c54fc1a5a84d8f6bab7a4ad675ae371
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-17 13:22:37 +0000

    esta es la actual que era del proyecto:studio-5708924923-1285a
    GEMINI_

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   projectId: 'divas-aa-80247010',
   appId: '1:914611598460:web:8d0b2f52c417c89f5f6537',
   storageBucket: 'divas-aa-80247010.appspot.com',
-  apiKey: 'AIzaSyDFP4k9oh9yP3sU1C-wL0Lp3gSPSqA1W2c',
+  apiKey: 'AIzaSyCIMXjDKeGYnnEc6eSD3R3zyJc8BnMy1o8',
   authDomain: 'divas-aa-80247010.firebaseapp.com',
   messagingSenderId: '914611598460',
 };
