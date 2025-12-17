9b36ccb7f3685717cc03d1726142368c3bd03c8f
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-17 04:11:37 +0000

    Error: Firebase: Error (auth/api-key-not-valid.-please-pass-a-valid-api-

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "divas-aa-80247010",
-  "appId": "1:914611598460:web:8d0b2f52c417c89f5f6537",
-  "storageBucket": "divas-aa-80247010.appspot.com",
-  "apiKey": "AIzaSyDFP4k9oh9yP3sU1C-wL0Lp3gSPSqA1W2c",
-  "authDomain": "divas-aa-80247010.firebaseapp.com",
-  "messagingSenderId": "914611598460"
+  projectId: 'divas-aa-80247010',
+  appId: '1:914611598460:web:8d0b2f52c417c89f5f6537',
+  storageBucket: 'divas-aa-80247010.appspot.com',
+  apiKey: 'AIzaSyDFP4k9oh9yP3sU1C-wL0Lp3gSPSqA1W2c',
+  authDomain: 'divas-aa-80247010.firebaseapp.com',
+  messagingSenderId: '914611598460',
 };
