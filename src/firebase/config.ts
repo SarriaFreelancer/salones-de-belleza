4ff0eefcb6c7f5096c760f8800ad77fe2bd31bcb
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-17 15:37:32 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  projectId: 'salon-de-belleza-5344218-af12a',
-  appId: '1:157310597400:web:89065c9e758c5b3173066d',
-  storageBucket: 'salon-de-belleza-5344218-af12a.appspot.com',
-  apiKey: 'AIzaSyC2q_xJ7v2L5C4n3B8f9G1hJk3r0D6yW9w',
-  authDomain: 'salon-de-belleza-5344218-af12a.firebaseapp.com',
-  messagingSenderId: '157310597400',
+  apiKey: "AIzaSyCfb7ePzr6pj_W7X_7d6O2csUFnCbVqBeI",
+  authDomain: "salon-de-belleza-5344218-af12a.firebaseapp.com",
+  projectId: "salon-de-belleza-5344218-af12a",
+  storageBucket: "salon-de-belleza-5344218-af12a.appspot.com",
+  messagingSenderId: "157310597400",
+  appId: "1:157310597400:web:89065c9e758c5b3173066d"
 };
