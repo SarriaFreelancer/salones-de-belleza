7ab982de16e26353429e67def52749acf9b26b1a
Juan David Cuero Sarria <davidjuanmontano7@gmail.com>
2025-12-23 14:49:13 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Failed to get docu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAidQZjB0mTktBFjXXquYoMSnzXp_LxplM",
   authDomain: "salondebelleza-d8734.firebaseapp.com",
   projectId: "salondebelleza-d8734",
-  storageBucket: "salondebelleza-d8734.appspot.com",
+  storageBucket: "salondebelleza-d8734.firebasestorage.app",
   messagingSenderId: "625029671652",
   appId: "1:625029671652:web:84850f650974f56a6fdaf1"
 };
